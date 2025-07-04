109d9e91ee20dfde068c56111ea2691edc72b4ec
Dylan Hanique <dylan.han82@gmail.com>
2025-07-04 15:00:09 +0200

    style: format codebase using ESLint and Prettier

--- app.vue
+++ app.vue
@@ -1,21 +1,21 @@
 <template>
-  <div>
-    <ProfileSection />
-    <hr>
-    <CareerSection />
-  </div>
+	<div>
+		<ProfileSection />
+		<hr />
+		<CareerSection />
+	</div>
 </template>
 
 <style scoped>
-div {
-  margin: 5% 20%;
-  padding: 5%;
-  border-radius: 0 25px 25px 25px;
-  background-color: rgb(255, 255, 255);
-}
+	div {
+		margin: 5% 20%;
+		padding: 5%;
+		border-radius: 0 25px 25px 25px;
+		background-color: rgb(255, 255, 255);
+	}
 
-hr {
-  margin: 7% 0;
-  color: #e9e9e9;
-}
+	hr {
+		margin: 7% 0;
+		color: #e9e9e9;
+	}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/ui',
-    '@nuxt/fonts'
-  ],
-  css: [
-    '@/assets/css/main.css'
-  ]
-})
+	compatibilityDate: '2025-05-15',
+	devtools: { enabled: true },
+	modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui', '@nuxt/fonts'],
+	css: ['@/assets/css/main.css'],
+});
