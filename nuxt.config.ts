9ceb13b60f1794c67392841da6f18c030a044d25
Dylan Hanique <dylan.han82@gmail.com>
2025-07-14 19:30:43 +0200

    feat: add app head, install @nuxt/images module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			title: 'Dylan Hanique',
+			htmlAttrs: {
+				lang: 'en',
+			},
+			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		},
+	},
+	image: {
+		dir: 'assets/images',
+	},
 	compatibilityDate: '2025-05-15',
 	devtools: { enabled: true },
-	modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui', '@nuxt/fonts'],
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/icon',
+		'@nuxt/ui',
+		'@nuxt/fonts',
+		'@nuxt/image',
+	],
 	css: ['@/assets/css/main.css'],
 });
