452ee1a6f35bc7efc3d056f3eb0202392f74b064
Dylan Hanique <dylan.han82@gmail.com>
2025-07-14 19:34:19 +0200

    refactor: replace pure CSS with Tailwind classes and style adjustments

--- components/ProfileSection.vue
+++ components/ProfileSection.vue
@@ -1,122 +1,64 @@
 <template>
-	<header>
-		<div class="left-section">
-			<img src="https://briceclain.com/img/profile.png" alt="" />
-			<h1>Dylan Hanique</h1>
-			<p>
+	<header class="grid grid-cols-12">
+		<div class="col-start-1 col-end-5">
+			<NuxtImg
+				id="profile-avatar"
+				preload
+				loading="lazy"
+				src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+				class="w-50 rounded-full object-cover shadow-lg"
+			/>
+			<h1 class="my-7 text-5xl leading-13 font-extrabold text-black">
+				Dylan <br />
+				Hanique
+			</h1>
+			<p class="mb-7 leading-none text-zinc-600">
 				A la recherche de mon <br />
 				premier contrat en freelance
 			</p>
-			<div class="social-icons">
-				<UIcon name="mdi-linkedin" />
-				<UIcon name="mdi-github" />
+			<div class="text-3xl text-black">
+				<UButton
+					icon="mdi-linkedin"
+					class="cursor-pointer text-3xl text-black hover:text-zinc-400"
+					variant="link"
+					to=""
+				/>
+				<UButton
+					icon="mdi-github"
+					class="cursor-pointer text-3xl text-black hover:text-zinc-400"
+					variant="link"
+					to=""
+				/>
 			</div>
 		</div>
 
-		<div class="right-section">
-			<h2><span class="first-word">Développeur</span> Full-Stack</h2>
-			<p>
+		<div class="right-section col-start-6 col-end-13 mt-5">
+			<h2 class="text-7xl leading-none font-extrabold text-zinc-400 uppercase">
+				<span class="text-black">Développeur</span><br />
+				Full-Stack
+			</h2>
+			<p class="mt-10 w-7/8 leading-5 text-zinc-600">
 				Passionné d'informatique depuis mon enfance, j'ai eu l'opportunité de
 				développer des applications dans des environnements très différents :
 				d'une petite entreprise d'assurance au plus près des besoins métier, à
 				un projet spatial d'envergure au sein d'une grande entreprise, où j'ai
 				contribué à des systèmes complexes et innovants.
 			</p>
-			<span class="years-experience">
-				<span class="first-letter">3</span> Ans d'Expérience
+			<span
+				class="years-experience my-10 flex items-center leading-4 font-medium text-black"
+			>
+				<span class="mr-1 text-5xl font-bold">3</span> Ans <br />d'Expérience
 			</span>
 			<div class="profile-buttons">
-				<UButton class="resume-button">Télécharger CV</UButton>
-				<UButton class="contact-button">Contact</UButton>
+				<UButton
+					class="resume-button mr-5 cursor-pointer rounded-full bg-white px-4 py-2 text-zinc-600 ring-2 ring-gray-800 hover:bg-gray-50"
+					>Télécharger CV</UButton
+				>
+				<UButton
+					class="contact-button cursor-pointer rounded-full bg-white px-4 py-2 text-zinc-600 ring-2 ring-gray-300 hover:bg-gray-50"
+					>Contact</UButton
+				>
 			</div>
 		</div>
 	</header>
 </template>
-
-<style scoped>
-	header {
-		display: grid;
-		grid-template-columns: repeat(12, 1fr);
-	}
-
-	.left-section {
-		grid-column: 1 / 5;
-	}
-
-	h1 {
-		font-size: 2.8rem;
-		line-height: 1.1;
-		font-weight: 800;
-		color: #000000;
-		margin: 7.5% 0 5% 0;
-		word-spacing: 100vw;
-	}
-
-	.left-section > p {
-		line-height: 1;
-		font-size: 1rem;
-		margin-bottom: 10%;
-		color: #6b6b6b;
-	}
-
-	.left-section > img {
-		width: 70%;
-		border-radius: 50%;
-	}
-
-	.social-icons span {
-		margin-right: 2%;
-		font-size: 1.8rem;
-		color: #000000;
-	}
-
-	.right-section {
-		margin-top: 5%;
-		grid-column: 6 / -1;
-	}
-
-	.right-section > p {
-		width: 85%;
-		color: #6b6b6b;
-		line-height: 1.1;
-	}
-
-	.years-experience {
-		display: flex;
-		align-items: center;
-		word-spacing: 100vw;
-		color: #000000;
-		line-height: 1.1;
-		font-weight: 500;
-	}
-
-	.first-letter {
-		font-size: 2.8rem;
-		font-weight: bold;
-		margin-right: 1%;
-	}
-
-	.right-section > p {
-		margin: 5% 0;
-	}
-
-	.profile-buttons {
-		margin-top: 7%;
-	}
-
-	.profile-buttons button {
-		border-radius: 50px;
-		margin-right: 3%;
-		background-color: #ffffff;
-		color: #3b3b3b;
-		padding: 2% 5%;
-	}
-
-	.resume-button {
-		border: 2px solid #020173;
-	}
-
-	.contact-button {
-		border: 2px solid #c3c3c3;
-	}
-</style>
